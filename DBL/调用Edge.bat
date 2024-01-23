34d18efc7ad19741f911b94621a38bcade473374
op200 <112297583+op200@users.noreply.github.com>
2024-01-24 04:50:04 +0800

    Delete DBL directory

--- DBL/调用Edge.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" %1
-exit
-жа
--- DBL/调用IE.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-start /min "" "C:\Program Files\Internet Explorer\iexplore.exe" %1
-exit
-中
-预计：添加输入%2以调用/min
